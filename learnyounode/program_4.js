53783008e84e9d6139bef2bf2cb314ea30e6229c
funada yu <y.funada@eneres.ltd>
2019-10-30 14:15:34 +0900

    LSのフィルター

--- learnyounode/program_4.js
+++ learnyounode/program_4.js
@@ -16,3 +16,21 @@ fs.readdir(dir, (err, list) => {
   });
 
 });
+
+
+// 'use strict'
+// const fs = require('fs')
+// const path = require('path')
+
+// const folder = process.argv[2]
+// const ext = '.' + process.argv[3]
+
+// fs.readdir(folder, function (err, files) {
+//   if (err) return console.error(err)
+//   files.forEach(function (file) {
+//     if (path.extname(file) === ext) {
+//       console.log(file)
+//     }
+//   })
+// })
+
