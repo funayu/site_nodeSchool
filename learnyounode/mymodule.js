53241265fd5c2b52ad1cfbb8d4f07eac77881bb0
funada yu <y.funada@eneres.ltd>
2019-10-31 17:16:14 +0900

    モジュラーにしよう　コメント追加、一部リファクタリング

--- learnyounode/mymodule.js
+++ learnyounode/mymodule.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
+// モジュール内に関数を一つ定義する
 module.exports = function (dir, filterStr, callback) {
+  // フォルダ内のファイル名のリストを取得する
   fs.readdir(dir, function (err, list) {
+    // エラーが発生した場合、コールバックにエラーを返す
     if (err) {
       return callback(err);
     }
 
-    list = list.filter(function (file) {
+    // ファイル名が引数の拡張子と同じものでフィルタして、新しい配列に入れる
+    const resultArr = list.filter((file) => {
       return path.extname(file) === '.' + filterStr;
     });
-
-    callback(null, list);
+    // リストを返す
+    callback(null, resultArr);
   });
 };
--- learnyounode/program_5.js
+++ learnyounode/program_5.js
@@ -1,14 +1,20 @@
+// モジュールの読み込み
 const filterFn = require('./mymodule.js');
 
+// 検索対象のフォルダパス
 const dir = process.argv[2];
+// 検索する拡張子
 const filterStr = process.argv[3];
 
+// モジュールの関数を呼び出す
+// モジュール側でエラーチェック、フィルタリング処理を実施する
 filterFn(dir, filterStr, function (err, list) {
+  // 処理の結果、エラーだった場合はコンソールにエラーを出力して処理を終了する
   if (err) {
     return console.error('エラーです！');
   }
-
-  list.forEach(function (file) {
+  // フィルタ済みリストの内容をコンソールに出力する
+  list.forEach((file) => {
     console.log(file);
   });
 });
