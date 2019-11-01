65c86d0133101d1f540b5783ae14a73478f170d6
funada yu <y.funada@eneres.ltd>
2019-11-01 16:47:41 +0900

    HTTP クライアント

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,20 +1,10 @@
-// モジュールの読み込み
-const filterFn = require('./mymodule.js');
+const http = require('http');
 
-// 検索対象のフォルダパス
-const dir = process.argv[2];
-// 検索する拡張子
-const filterStr = process.argv[3];
+const url = process.argv[2];
 
-// モジュールの関数を呼び出す
-// モジュール側でエラーチェック、フィルタリング処理を実施する
-filterFn(dir, filterStr, function (err, list) {
-  // 処理の結果、エラーだった場合はコンソールにエラーを出力して処理を終了する
-  if (err) {
-    return console.error('エラーです！');
-  }
-  // フィルタ済みリストの内容をコンソールに出力する
-  list.forEach((file) => {
-    console.log(file);
+http.get(url, (response) => {
+  response.setEncoding('utf-8');
+  response.on('data', (data) => {
+    console.log(data);
   });
 });
